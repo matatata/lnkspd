1dd96958e205a5fd89feb27a3f4478ce9db38ec9
Matteo Ceruti <31549147+matatata@users.noreply.github.com>
2019-06-16 19:14:20 +0200

    Update make.sh

--- make.sh
+++ make.sh
@@ -1,5 +1,6 @@
 
+# Use my fork https://github.com/matatata/pciutils, otherwise the libpci.a lib will not be present
+PCIUTILS_DIR="/Users/matteo/Downloads/pciutils/"
 
-
-gcc -lpci  -L/Users/matteo/Downloads/pciutils/lib -I/Users/matteo/Downloads/pciutils/ -framework IOKit main.c -o lnkspd
+gcc -lpci  -L"$PCIUTILS_DIR/lib" -I"$PCIUTILS_DIR" -framework IOKit main.c -o lnkspd
 
