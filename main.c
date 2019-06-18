94339c4a697874e00cadcd439042213fdeb16acc
Matatata <matatata@me.com>
2019-06-19 00:23:20 +0200

    use libpci and DirectHW framework

--- main.c
+++ main.c
@@ -11,6 +11,7 @@
 #include <ctype.h>
 #include <unistd.h>
 
+
 #include <pciutils.h>
 
 
--- make.sh
+++
@@ -1,6 +0,0 @@
-
-# Use my fork https://github.com/matatata/pciutils, otherwise the libpci.a lib will not be present
-PCIUTILS_DIR="/Users/matteo/Downloads/pciutils/"
-
-gcc -lpci  -L"$PCIUTILS_DIR/lib" -I"$PCIUTILS_DIR" -framework IOKit main.c -o lnkspd
-
